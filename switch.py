18b70cdb1f084747a0c962ef3f360e6cc9d81386
Manuel Bonk <m@bonk.io>
2014-05-31 23:37:32 +0200

    new file:   raspberry-remote 	modified:   switch.py

--- switch.py
+++ switch.py
@@ -3,19 +3,7 @@
 # import required libraries
 from bottle import route, run, get, static_file
 from subprocess import call
-import time
 
-def setSwitch(switch, state):
-    return "penis"
-
-""" @route('/switch/<name>/<pos>')
- def index(name, pos):
-   name = name.upper()
-  pos = pos.upper()
-  setSwitch(name, pos)
-  return '<!doctype html><html><p style="font-family:sans-serif">%s</p></html>' % (pos)
-  return 'Switch %s %s</b>' % (name, pos)
-"""
 @route ('/')
 def index ():
     output = open ('index.html')
@@ -27,7 +15,6 @@ def server_static(filename):
         return static_file(filename,root='/home/m/dev/remote/static')
 
 @route ('/switch/<code>/<pin>/<pos>')
-# def call(["/home/m/dev/raspberry-remote/send",code,pin,pos])
 def switch(code,pin,pos):
     call(["send",code,pin,pos])
     state="off"

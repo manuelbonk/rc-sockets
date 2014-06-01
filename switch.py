a8175b6f3c4466957ea5ff53810e5d187c7e784b
Manuel Bonk <m@bonk.io>
2014-06-01 02:09:10 +0200

    iframe style modified 	modified:   switch.py

--- switch.py
+++ switch.py
@@ -4,29 +4,24 @@
 from bottle import route, run, get, static_file, redirect
 from subprocess import call
 
-"""
-@route ('/')
-def index ():
-    return static_file("index.html",root='/home/m/dev/remote/static/')
-#    output = open ('/home/m/dev/remote/static/index.html')
-#    return output
-"""
 @route('/')
 @route('index.html')
 def index():
-    return static_file("index.html",root='/home/m/dev/remote/')
+    return static_file("index.html",root='/home/m/dev/remote/static')
 
 # static files
 @route('/static/<filename>')
 def server_static(filename):
         return static_file(filename,root='/home/m/dev/remote/static')
 
-@route ('/switch/<code>/<pin>/<pos>')
-def switch(code,pin,pos):
+@route ('/<mode>/<code>/<pin>/<pos>')
+def switch(mode,code,pin,pos):
     call(["./rcswitch-pi/send",code,pin,pos])
     state="off"
     if pos == "1":
         state="on"
-    return '<!doctype html><html><p style="font-family:sans-serif">code: %s<br/>turned switch %s %s</p></html>' % (code,pin,state)
+    if mode == "verbose":
+        return '<!doctype html><html><p style="font-family:sans-serif;font-size:18px">code: %s<br/>turned switch %s %s</p></html>' % (code,pin,state)
+    return '<!doctype html><html><p style="font-family:sans-serif;font-size:18px">%s</p></html>' % (state)
 
 run (host='0.0.0.0', port=8080)

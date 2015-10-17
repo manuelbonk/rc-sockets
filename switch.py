68c862c95fc0105ae2532d6612f67fd09a5a3335
Manuel Bonk <github@bonkii.com>
2015-10-17 14:13:22 +0200

    set the static dir's absolute path to a relative one

--- switch.py
+++ switch.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
 # import required libraries
-from bottle import route, run, get, static_file, redirect
+from bottle import route, run, get, static_file, redirect, os
 from subprocess import call
 
 @route('/')
 @route('index.html')
 def index():
-    return static_file("index.html",root='/home/m/dev/remote/static/')
+    return static_file("index.html",root=cwd+'./static/')
 
 # static files
 @route('/static/<filename>')
 def server_static(filename):
-        return static_file(filename,root='/home/m/dev/remote/static/')
+        return static_file(filename,root=cwd+'./static/')
 
 @route ('/<mode>/<code>/<pin>/<pos>')
 def switch(mode,code,pin,pos):
@@ -24,4 +24,6 @@ def switch(mode,code,pin,pos):
         return '<!doctype html><html><p style="font-family:sans-serif;font-size:18px">code: %s<br/>turned switch %s %s</p></html>' % (code,pin,state)
     return '<!doctype html><html><p style="font-family:sans-serif;font-size:18px">%s</p></html>' % (state)
 
+cwd=os.path.dirname(os.path.abspath(__file__))+"/"
+print(cwd)
 run (host='0.0.0.0', port=80)
